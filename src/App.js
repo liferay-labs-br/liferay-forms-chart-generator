02a69a1ebd2d5cb296f13f301bfb93d24fa5745a
Bruno Basto <bruno.basto@liferay.com>
2018-01-18 11:33:00 -0200

    Source formatting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import templates from './App.soy.js';
 import Component from 'metal-component';
-import {Config} from 'metal-state';
 import Soy from 'metal-soy';
 
 import './app.scss';
 
-class App extends Component {
-}
+/**
+ * App class
+ */
+class App extends Component {}
 
 Soy.register(App, templates);
 
-export { App };
+export {App};
 export default App;
